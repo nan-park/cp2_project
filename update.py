91ccdb5b5efc7e7b554c47d94dd62f4067f90995
NanPark <53233842+nan-park@users.noreply.github.com>
2023-02-10 18:14:04 +0900

    Delete update.py

--- update.py
+++
@@ -1,7 +0,0 @@
-from config import CFG
-import model_builder
-
-s_bert = model_builder.load_sbert(model_path=CFG.CLASSIFIER_MODEL_PATH,
-                                  sbert_path=CFG.SBERT_MODEL_FOLDER)
-classifier = model_builder.get_classifier()
-classifier.load_weights()
